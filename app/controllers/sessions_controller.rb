b8e7742ac44266ade5dec58e4546b826f4c85d6a
Arthur <arthur.p.wright@gmail.com>
2015-04-23 16:05:19 -0400

    Built Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_url(user)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url
+  end
+
 end
