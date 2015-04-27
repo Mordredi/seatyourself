3eb5684b5ba869b1d1d386a75ebc05a859c2ffac
Arthur <arthur.p.wright@gmail.com>
2015-04-27 17:55:21 -0400

    Styled flash notices

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -6,7 +6,6 @@ class ReviewsControllerTest < ActionController::TestCase
     @user = User.create(first_name: 'Frodo', last_name: 'Baggins', email: 'bilbo@lotr.net', password: 'yahoo!', password_confirmation: 'yahoo!')
     assert_difference('Review.count', +1) do
       post :create, review: {title: 'Test', content: 'Testing testing' }, restaurant_id: @restaurant.id, user_id: @user.id
-      binding.pry
     end
 
     assert_redirected_to restaurant_url(@restaurant)
